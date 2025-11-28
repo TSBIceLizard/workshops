10ca8c4540b49bd8ac72b66c84deed47aea2cf85
arronburgess <generalkajgue@hotmail.com>
2025-11-28 16:13:24 +0000

    feat: variables

---
+++ week-01/06-js-variables/app.js
@@ -0,0 +1,16 @@
+// TODO: I want to log a page input to the console.
+console.log("Hello World!");
+
+//Using camel-case
+let myName = "Arron";
+let boolLight = true; //booleans either use true or false values. That is all they can do
+let myAge = 36;
+//You can never 're'declare a variable later on in the code.
+//the let declaration allows the variable to be changed, whereas const is rigid.
+
+//console.log("My name is " + myName + " and I am " + myAge + " years young!!");
+
+//TODO: Log my name and age to the console
+
+//extra: template literals
+console.log(`My name is ${myName} and I am ${myAge} years young!!`);
